7c4bb8153018d5631eac40f5047151e5e74d3492
Anastasia-code <52262297+Anastasia-code@users.noreply.github.com>
2019-07-02 10:09:40 -0700

    Practice with lists

---
+++ WomenInTechList.py
@@ -0,0 +1,39 @@
+friends = ["Randles", "Vaughan", "Lee", "Alvarez", "Ericsson"]
+ages = [30, 40, 24, 35, 39]
+random_dat = ["Woman", 16, 13]
+combined = [random_dat,ages,"Randles", "Vaughan", "Lee", "Alvarez", "Ericsson"]
+
+print(friends)
+# print(friends[0:2])
+# object=slice("Vaughan","Alvarez")  #This slicing is wrong 
+# print(friends[object])
+
+
+# print(friends)
+# print(len(friends))
+# print("Randles" in friends)
+# print("Hi" in friends)
+# print(friends[0])
+#
+#
+# for item in friends:
+#     print(item)
+#
+# print()
+#
+# for i in range(len(friends)):
+#     print(friends[i])
+#
+# print()
+# print(friends[0]+ " " + friends[2])
+#
+#
+# anExample="Ada!"
+# print(len(anExample))
+# for letter in anExample:
+#     print(letter)
+#
+# print(random_dat)
+# print(combined)
+#
+#
